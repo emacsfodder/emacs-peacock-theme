9c4beebfb1772ff218a1a6d80ba899a40348a49a
Jasonm23 <jasonm23@gmail.com>
2014-11-16 14:30:33 +0800

    Update author and version keys

--- peacock-theme.el
+++ peacock-theme.el
@@ -1,8 +1,8 @@
 ;;; peacock-theme.el --- an Emacs 24 theme based on Peacock (tmTheme)
 ;;
-;;; Author: Auto Converted to Emacs 24 by tmtheme-to-deftheme (tm2deftheme)
-;;; Version: 1416104800
-;;; Original author: 
+;;; Author: Jason Milkins
+;;; Version: 20141116
+ 
 ;;; Url: https://github.com/emacsfodder/tmtheme-to-deftheme
 ;;; Package-Requires: ((emacs "24.0"))
 ;;
